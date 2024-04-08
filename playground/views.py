2b059d38873caf8d986950b10996ea24139c31b3
Timo de Jong <tjdejong98@gmail.com>
2024-04-08 20:44:49 +0200

    Playing around with objects

--- playground/views.py
+++ playground/views.py
@@ -1,18 +1,31 @@
 from django.shortcuts import render
 from django.contrib.contenttypes.models import ContentType
-from store.models import Product
+from store.models import Product, Collection
 from tags.models import TaggedItem
 
 # Create your views here.
 
 
 def say_hello(request):
-    content_type = ContentType.objects.get_for_model(Product)
+    # collection = Collection(pk=11)
+    # collection.delete()
 
-    quaryset = TaggedItem.objects \
-        .select_related('tag') \
-        .filter(
-            content_type=content_type,
-            object_id=1
-        )
-    return render(request, 'hello.html', {'name': 'timo', 'tags': list(quaryset)})
+    Collection.objects.filter(id__gt=12).delete()
+
+    # collection = Collection.object.get(pk=11)
+    # collection.title = 'Games'
+    # collection.featured_product = None
+    # collection.save()
+
+    # Problem with changing names of for example "Freatured_product" but no pre loading needed so faster
+    # Collection.objects.filter(pk=11).update(featured_product=None)
+
+    # quaryset = Product.objects.all()
+    # writes to quaryset cache
+    # list(quaryset)
+    # gets from cache
+    # list(quaryset)
+
+    # TaggedItem.objects.get_tags_for(Product, 1)
+
+    return render(request, 'hello.html', {'name': 'timo', 'tags': list()})
--- tags/models.py
+++ tags/models.py
@@ -2,19 +2,33 @@ from django.db import models
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 
+
+class TaggedItemManager(models.Manager):
+    def get_tags_for(self, obj_type, obj_id):
+        content_type = ContentType.objects.get_for_model(obj_type)
+
+        return TaggedItem.objects \
+            .select_related('tag') \
+            .filter(
+                content_type=content_type,
+                object_id=obj_id
+            )
+
 # Create your models here.
+
+
 class Tag(models.Model):
     label = models.CharField(max_length=254)
 
 
 class TaggedItem(models.Model):
+    objects = TaggedItemManager()
     # What tag is applied to what object
     tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
-    
+
     # this is how to do a generic app
     # Type (product, video, article)
     # ID
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey()
-    
